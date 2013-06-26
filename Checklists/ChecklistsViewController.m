f9112f0001962e90309e0830b84f1776f72af5ab
Claus Guttesen <kometen@gmail.com>
2013-06-26 23:46:21 +0200

    Revert "Refactor classname."
    This reverts commit 391cf259546a4de8d342683a4a7375cf3dde6b5d.

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2013 Claus Guttesen. All rights reserved.
 //
 
-#import "ChecklistViewController.h"
+#import "ChecklistsViewController.h"
 #import "ChecklistItem.h"
 
-@interface ChecklistViewController ()
+@interface ChecklistsViewController ()
 
 @end
 
-@implementation ChecklistViewController {
+@implementation ChecklistsViewController {
     NSMutableArray *items;
 }
 
