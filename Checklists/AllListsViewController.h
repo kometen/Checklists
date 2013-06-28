c778d3ef8b3d93276e3c62306f442423c7267638
Claus Guttesen <kometen@gmail.com>
2013-06-28 23:17:55 +0200

    Added a new table view controller. Call detail view with a segue.

---
+++ Checklists/AllListsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllListsViewController.h
+//  Checklists
+//
+//  Created by Claus Guttesen on 28/06/13.
+//  Copyright (c) 2013 Claus Guttesen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllListsViewController : UITableViewController
+
+@end
